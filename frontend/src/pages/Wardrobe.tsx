1b686eb249363838cff7006e8f537fb98a5c9c46
Sarone Kelete <skelete@ucsc.edu>
2025-11-12 11:47:47 -0800

    view-details

--- frontend/src/pages/Wardrobe.tsx
+++ frontend/src/pages/Wardrobe.tsx
@@ -329,6 +329,7 @@ const Wardrobe: React.FC = () => {
                   favorite={!!it.favorite}
                   onToggleFavorite={toggleFavorite}
                   onDelete={deleteItem}
+                  onViewDetails={handleViewDetails}
                 />
               </div>
             ))}
